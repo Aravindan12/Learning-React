1c97fccb7933d42592fa39dc6bef165e7b7f7743
Aravindan12 <aravind@sparkouttech.com>
2023-07-20 09:29:31 +0530

    arrow function based on es6 syntax on functional component

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello Aravind</h1>
-}
+// function Greet() {
+//     return <h1>Hello Aravind</h1>
+// }
+const Greet = () => <h1>Hello Aravind</h1> //arrow function based on es6 syntax
 
 export default Greet
